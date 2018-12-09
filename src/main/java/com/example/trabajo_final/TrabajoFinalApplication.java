0da73a04b42fc95d7787a81c2d617518b69c40d8
Melisa <melissalantigua@hotmail.com>
2018-12-09 16:38:13 -0400

    first commit

---
+++ src/main/java/com/example/trabajo_final/TrabajoFinalApplication.java
@@ -0,0 +1,12 @@
+package com.example.trabajo_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabajoFinalApplication.class, args);
+    }
+}
