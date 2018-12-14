f1b929ca84ab3d88036105a2322e205f47b9c4df
Juan Joa <juandanieljoa@gmail.com>
2018-12-14 10:18:50 -0400

    Constructores y getters and setters

--- src/main/java/com/example/trabajo_final/entidades/clientes.java
+++ src/main/java/com/example/trabajo_final/entidades/clientes.java
@@ -18,4 +18,95 @@ public class clientes {
     private String direccion_envio;
     private Long cuenta_cancaria;
 
+    public clientes(long cedula, String nombre, String apellido, String username, String password, Date birth_date, String genero, String direccion_envio, Long cuenta_cancaria) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.username = username;
+        this.password = password;
+        this.birth_date = birth_date;
+        this.genero = genero;
+        this.direccion_envio = direccion_envio;
+        this.cuenta_cancaria = cuenta_cancaria;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(long cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getBirth_date() {
+        return birth_date;
+    }
+
+    public void setBirth_date(Date birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getDireccion_envio() {
+        return direccion_envio;
+    }
+
+    public void setDireccion_envio(String direccion_envio) {
+        this.direccion_envio = direccion_envio;
+    }
+
+    public Long getCuenta_cancaria() {
+        return cuenta_cancaria;
+    }
+
+    public void setCuenta_cancaria(Long cuenta_cancaria) {
+        this.cuenta_cancaria = cuenta_cancaria;
+    }
 }
--- src/main/java/com/example/trabajo_final/entidades/factura.java
+++ src/main/java/com/example/trabajo_final/entidades/factura.java
@@ -7,4 +7,34 @@ public class factura {
     public long id;
     public clientes cliente;
     public Set<articulos> items;
+
+    public factura(long id, clientes cliente, Set<articulos> items) {
+        this.id = id;
+        this.cliente = cliente;
+        this.items = items;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public clientes getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(clientes cliente) {
+        this.cliente = cliente;
+    }
+
+    public Set<articulos> getItems() {
+        return items;
+    }
+
+    public void setItems(Set<articulos> items) {
+        this.items = items;
+    }
 }
