3fda35977e43b05c101fab79f41815be98f3293f
Melisa <melissalantigua@hotmail.com>
2018-12-17 16:52:58 -0400

    agregado template

--- src/main/java/com/example/trabajo_final/config/SecurityConfig.java
+++ src/main/java/com/example/trabajo_final/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.example.trabajo_final.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+/import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
