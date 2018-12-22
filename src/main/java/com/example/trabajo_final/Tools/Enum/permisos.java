689503d25c66ba058f3482dcf0139326db8d42a7
Juan Joa <juandanieljoa@gmail.com>
2018-12-22 14:43:42 -0400

    repo de usuario y roles, servicio de usuario, updating security config

--- src/main/java/com/example/trabajo_final/Tools/Enum/permisos.java
+++ src/main/java/com/example/trabajo_final/Tools/Enum/permisos.java
@@ -1,8 +1,37 @@
 package com.example.trabajo_final.Tools.Enum;
 
-public enum permisos {
+import javax.persistence.*;
 
-    ADMIN,
-    C0MPANY,
-    CONSUMER
+@Entity
+@Table(name = "role")
+public class permisos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private int id;
+    private String rol;
+
+    public permisos(String rol) {
+        this.rol = rol;
+    }
+
+    public permisos() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
 }
--- src/main/java/com/example/trabajo_final/config/SecurityConfig.java
+++ src/main/java/com/example/trabajo_final/config/SecurityConfig.java
@@ -1,21 +1,41 @@
 package com.example.trabajo_final.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Value("${spring.queries.users-query}")
+    private String usersQuery;
+
+    @Value("${spring.queries.roles-query}")
+    private String rolesQuery;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                    .antMatchers("/").permitAll()
+                    .antMatchers("/login").permitAll()
                     .antMatchers("/css/**", "/index").permitAll()
                     .antMatchers("/user/**").hasRole("USER")
+                    .antMatchers("/user/**").hasRole("ADMIN")
                     .antMatchers("/").hasRole("USER")
+                    .antMatchers("/").hasRole("ADMIN")
                     .and()
                 .formLogin()
                     .loginPage("/login").failureUrl("/login-error")
