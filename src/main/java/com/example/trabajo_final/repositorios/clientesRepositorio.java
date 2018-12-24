880081cecf11dc3cd4c75c15a26694b3c701e804
Melisa <melissalantigua@hotmail.com>
2018-12-24 17:31:18 -0400

    agregado repositorio faltantes

--- src/main/java/com/example/trabajo_final/repositorios/clientesRepositorio.java
+++ src/main/java/com/example/trabajo_final/repositorios/clientesRepositorio.java
@@ -1,4 +1,11 @@
 package com.example.trabajo_final.repositorios;
 
-public class clientesRepositorio {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.trabajo_final.entidades.*;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface clientesRepositorio extends JpaRepository<clientes,Long>{
+    @Query("select u from clientes u where u.id = :id")
+    clientes buscar(@Param("id") Long id);
 }
--- src/main/java/com/example/trabajo_final/repositorios/facturasRepositorio.java
+++ src/main/java/com/example/trabajo_final/repositorios/facturasRepositorio.java
@@ -1,4 +1,11 @@
 package com.example.trabajo_final.repositorios;
 
-public class facturasRepositorio {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.trabajo_final.entidades.*;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface facturasRepositorio extends JpaRepository<factura,Long>{
+    @Query("select u from factura u where u.id = :id")
+    factura buscar(@Param("id") Long id);
 }
