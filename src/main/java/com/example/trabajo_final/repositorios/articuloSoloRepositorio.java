8f306ada41aa8f1caa204689681c9319d5b21e40
Melisa <melissalantigua@hotmail.com>
2018-12-24 17:27:33 -0400

    agregado repositorio articulos

--- src/main/java/com/example/trabajo_final/repositorios/articuloSoloRepositorio.java
+++ src/main/java/com/example/trabajo_final/repositorios/articuloSoloRepositorio.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface articuloSoloRepositorio extends JpaRepository<articuloSolo,Long> {
-    @Query("select u from articulo_solo u where u.id = :id")
+    @Query("select u from articuloSolo u where u.id = :id")
     articuloSolo buscar(@Param("id") Long id);
 }
--- src/main/java/com/example/trabajo_final/repositorios/articulosRepositorio.java
+++ src/main/java/com/example/trabajo_final/repositorios/articulosRepositorio.java
@@ -1,4 +1,11 @@
 package com.example.trabajo_final.repositorios;
 
-public class articulosRepositorio {
+import com.example.trabajo_final.entidades.*;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface articulosRepositorio extends JpaRepository<articulos,Long> {
+    @Query("select u from articulos u where u.id = :id")
+    articulos buscar(@Param("id") Long id);
 }
