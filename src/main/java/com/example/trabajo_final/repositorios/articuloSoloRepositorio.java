278afe3cef6f88c504bfcac5316d60a481d8b4c8
Melisa <melissalantigua@hotmail.com>
2018-12-24 17:11:49 -0400

    agregado repositorio

---
+++ src/main/java/com/example/trabajo_final/repositorios/articuloSoloRepositorio.java
@@ -0,0 +1,11 @@
+package com.example.trabajo_final.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.trabajo_final.entidades.*;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface articuloSoloRepositorio extends JpaRepository<articuloSolo,Long> {
+    @Query("select u from articulo_solo u where u.id = :id")
+    articuloSolo buscar(@Param("id") Long id);
+}
---
+++ src/main/java/com/example/trabajo_final/repositorios/clientesRepositorio.java
@@ -0,0 +1,4 @@
+package com.example.trabajo_final.repositorios;
+
+public class clientesRepositorio {
+}
