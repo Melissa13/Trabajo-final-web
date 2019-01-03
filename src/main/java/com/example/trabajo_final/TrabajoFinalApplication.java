f8d3fe3f9ecd40ff44132e354f909d55162c8679
Melisa <melissalantigua@hotmail.com>
2019-01-03 11:57:44 -0400

    cambio nombre y valores Usuario

--- src/main/java/com/example/trabajo_final/TrabajoFinalApplication.java
+++ src/main/java/com/example/trabajo_final/TrabajoFinalApplication.java
@@ -1,7 +1,5 @@
 package com.example.trabajo_final;
 
-import com.example.trabajo_final.entidades.clientes;
-import com.example.trabajo_final.repositorios.clientesRepositorio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -17,22 +15,5 @@ public class TrabajoFinalApplication {
 
         ApplicationContext applicationContext = SpringApplication.run(TrabajoFinalApplication.class, args);
 
-        clientesRepositorio clientRep = (clientesRepositorio) applicationContext.getBean("clientesRepositorio");
-
-        /*Long l=123L;
-        Date today = Calendar.getInstance().getTime();
-        clientes c=new clientes();
-        c.setBirth_date(today);
-        c.setCedula(l);
-        c.setGenero("Masculino");
-        c.setNombre("Pedro");
-        c.setDireccion_envio("Santiago");
-        clientRep.save(c);*/
-
-        List<clientes> client= clientRep.findAll();
-        System.out.println(" ----CLIENTES---");
-        for (clientes cc:client){
-            System.out.println("NOMBRE:"+cc.getNombre()+" ID:"+cc.getId()+" NACIMIENTO: "+ cc.getBirth_date()+" CEDULA:"+ cc.getCedula()+" LUGAR: "+cc.getDireccion_envio());
-        }
     }
 }
