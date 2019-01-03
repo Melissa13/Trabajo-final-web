f6ccff10378b739c448a9e95ad6b89e042163725
Melisa <melissalantigua@hotmail.com>
2019-01-03 14:09:31 -0400

    agregado repositorio de factura

---
+++ src/main/java/com/example/trabajo_final/Repositorio/FacturaRepositorio.java
@@ -0,0 +1,23 @@
+package com.example.trabajo_final.Repositorio;
+
+import com.example.trabajo_final.Tools.Enum.EstadoEnvio;
+import com.example.trabajo_final.entidades.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public interface FacturaRepositorio extends JpaRepository<Factura, Long> {
+    Factura findByFiscalCode(String fiscalCode);
+
+    @Query("select r from Factura r where r.user.email = :email")
+    List<Factura> findByUser(@Param("email") String email);
+
+    @Query("select r from Factura r where r.status = :status")
+    List<Factura> findByOrderStatus(@Param("status") EstadoEnvio status);
+
+    @Query("select r from Factura r where r.transactionDate between :beginning and :ending")
+    List<Factura> findByTimestamp(@Param("beginning") Timestamp start, @Param("ending") Timestamp end);
+}
