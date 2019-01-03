a9cfc989fd28e219af43b20433705ae86e48c844
Juan Joa <Juandanieljoa@gmail.com>
2019-01-03 11:45:01 -0400

    Delete RolRepository.java

--- src/main/java/com/example/trabajo_final/repositorios/RolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.trabajo_final.repositorios;
-/*import com.example.trabajo_final.Tools.Enum.permisos;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("roleRepository")
-public interface permisoRepository extends JpaRepository<permisos, Integer>{
-    permisos findByRol(String rol);
-}*/
